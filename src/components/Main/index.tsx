96f3b5fbd68edfd6d1e1d1a80146d1a5a764a58c
Adeonir Kohl <adeonir@gmail.com>
2020-07-25 15:07:07 -0300

    Add first test

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,5 @@
+export const Main = () => (
+  <main>
+    <h1>React Avançado</h1>
+  </main>
+)
---
+++ src/components/Main/test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react'
+
+import { Main } from '.'
+
+describe('<Main />', () => {
+  it('should render the heading', () => {
+    const {
+      container: { firstChild },
+    } = render(<Main />)
+
+    expect(
+      screen.getByRole('heading', { name: /react avançado/i })
+    ).toBeInTheDocument()
+
+    expect(firstChild).toMatchSnapshot()
+  })
+})
