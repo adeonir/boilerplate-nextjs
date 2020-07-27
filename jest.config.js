5a9d3e26c7989003cb78f4784af0512653b6efe3
Adeonir Kohl <adeonir@gmail.com>
2020-07-27 17:43:04 -0300

    Add jest module paths

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/config.ts'],
+  modulePaths: ['<rootDir>/src'],
 }
