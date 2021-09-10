3d5c78c4302de0af16332784ab31df2db50e0dad
Adeonir Kohl <adeonir@gmail.com>
2021-09-10 15:03:35 -0300

    chore: update storybook to use webpack 5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,7 @@
 module.exports = {
   stories: ['../src/components/**/stories.tsx'],
   addons: ['@storybook/addon-essentials', 'storybook-addon-next-router'],
+  core: { builder: 'webpack5' },
   babel: async (options) => ({
     ...options,
     plugins: [
