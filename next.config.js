6b9d0c2e48d0fa6ae64871494ae0a4b65ed0e3ba
Adeonir Kohl <adeonir@gmail.com>
2021-09-24 14:37:43 -0300

    chore: update next config to use preact on prod

--- next.config.js
+++ next.config.js
@@ -1,11 +1,25 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-/* eslint-disable no-undef */
 const withPWA = require('next-pwa')
 const isProduction = process.env.NODE_ENV === 'production'
 
 module.exports = withPWA({
+  target: 'serverless',
   pwa: {
     dest: 'public',
     disable: !isProduction,
   },
+  images: {
+    domains: ['localhost'],
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      })
+    }
+
+    return config
+  },
+  generateBuildId: () => 'build',
 })
