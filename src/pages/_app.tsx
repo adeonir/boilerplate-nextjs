0039f8f7495f0735a8776b218b1c3d1f78c09327
Adeonir Kohl <adeonir@gmail.com>
2022-04-16 11:15:27 -0300

    Fix lint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,9 +9,18 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>React Avançado</title>
-        <link rel="shortcut" href="/assets/icon-512.png" />
-        <link rel="apple-touch-icon" href="/assets/icon-512.png" />
-        <link rel="manifest" href="/manifest.json" />
+        <link
+          rel="shortcut"
+          href="/assets/icon-512.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/assets/icon-512.png"
+        />
+        <link
+          rel="manifest"
+          href="/manifest.json"
+        />
         <meta
           name="description"
           content="A simple project to work with NextJS, React, TypeScript and Styled-Components"
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -40,8 +40,14 @@ export default class MyDocument extends Document {
     return (
       <Html lang="pt-BR">
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.googleapis.com"
+          />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+          />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700&display=swap"
