aeb205d00051e21000eae16ed50af0881872a8c9
Adeonir Kohl <adeonir@gmail.com>
2023-09-07 11:07:42 -0300

    update eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,48 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "next/core-web-vitals"
+  ],
+  "overrides": [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        ".eslintrc.{js,cjs}"
+      ],
+      parserOptions: {
+        "sourceType": "script"
+      }
+    }
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module"
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  plugins: [
+    "@typescript-eslint",
+    "react"
+  ],
+  rules: {
+    "@typescript-eslint/consistent-type-imports": "error",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "react-hooks/exhaustive-deps": "warn",
+    "react-hooks/rules-of-hooks": "error",
+    "react/display-name": "off",
+    "react/prop-types": "off"
+  }
+}
