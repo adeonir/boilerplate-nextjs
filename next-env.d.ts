ba9e789fb701ce67834e6025944ddf7c76181951
Adeonir Kohl <adeonir@gmail.com>
2023-09-08 18:31:07 -0300

    fix jest-dom types

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
+/// <reference types="@testing-library/jest-dom" />
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
