70fc1d41910d08e793d1afcd20a791e105e99049
Adeonir Kohl <adeonir@gmail.com>
2023-09-07 22:20:32 -0300

    install and setup prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,47 +2,43 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
   },
-  "extends": [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "next/core-web-vitals"
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'next/core-web-vitals',
+    'plugin:prettier/recommended',
   ],
-  "overrides": [
+  overrides: [
     {
       env: {
-        node: true
+        node: true,
       },
-      files: [
-        ".eslintrc.{js,cjs}"
-      ],
+      files: ['.eslintrc.cjs', '.prettierrc.cjs'],
       parserOptions: {
-        "sourceType": "script"
-      }
-    }
+        sourceType: 'script',
+      },
+    },
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module"
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   settings: {
     react: {
-      version: 'detect'
-    }
+      version: 'detect',
+    },
   },
-  plugins: [
-    "@typescript-eslint",
-    "react"
-  ],
+  plugins: ['@typescript-eslint', 'react'],
   rules: {
-    "@typescript-eslint/consistent-type-imports": "error",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "react-hooks/exhaustive-deps": "warn",
-    "react-hooks/rules-of-hooks": "error",
-    "react/display-name": "off",
-    "react/prop-types": "off"
-  }
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'error',
+    'react/display-name': 'off',
+    'react/prop-types': 'off',
+  },
 }
---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  semi: false,
+  printWidth: 120,
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'always',
+  tailwindConfig: './tailwind.config.ts',
+  plugins: ['prettier-plugin-tailwindcss']
+}
