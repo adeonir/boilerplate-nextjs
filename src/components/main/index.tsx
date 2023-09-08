95c04ba03e8075a685ae2fdb46074479355e6918
Adeonir Kohl <adeonir@gmail.com>
2023-09-08 08:51:16 -0300

    update styles on main component

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -7,16 +7,18 @@ export type Props = {
 
 export const Main = ({ title, description }: Props) => {
   return (
-    <main className="flex h-full flex-col items-center justify-center gap-4 bg-blue-950 p-16 text-center text-white">
-      <h1>{title}</h1>
-      <h2>{description}</h2>
+    <main className="flex h-full flex-col items-center justify-center gap-16 bg-blue-950 p-16 text-center text-white">
+      <div className="flex flex-col items-center justify-center gap-4">
+        <h1 className="text-4xl font-bold">{title}</h1>
+        <h2 className="text-xl">{description}</h2>
+      </div>
 
       <Image
         width="432"
         height="328"
         src="/assets/hero-illustration.svg"
         alt="A developer walking and a screen with the text NextJS"
-        className="w-full min-w-10"
+        className="w-full min-w-10 max-w-2xl"
       />
     </main>
   )
