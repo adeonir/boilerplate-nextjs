c831042265d1b74926246ac7954954911d70b840
Adeonir Kohl <adeonir@gmail.com>
2023-09-08 10:19:43 -0300

    create plop templates

--- plopfile.js
+++ plopfile.js
@@ -15,7 +15,27 @@ module.exports = async (plop) => {
         },
       },
     ],
-    actions: [],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/components/{{kebabCase name}}/index.tsx',
+        templateFile: '.plop/templates/index.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{kebabCase name}}/stories.tsx',
+        templateFile: '.plop/templates/stories.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{kebabCase name}}/test.tsx',
+        templateFile: '.plop/templates/test.tsx.hbs',
+      },
+      {
+        type: 'eslint',
+        path: 'src/components/{{kebabCase name}}/*.tsx',
+      },
+    ],
   }),
     await plop.load('plop-action-eslint')
 }
