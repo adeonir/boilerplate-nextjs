baf4d982c9ee78d5c60095d0d1cf5dee6d3d186f
Adeonir Kohl <adeonir@gmail.com>
2023-09-11 15:34:23 -0300

    fix jest-dom types

--- vitest.config.ts
+++ vitest.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
     coverage: {
       provider: 'v8',
       reporter: ['html'],
-      include: ['src/**/*.{ts,tsx}'],
+      include: ['src/**/*.tsx'],
     },
   },
 })
--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,4 +1,4 @@
-import '@testing-library/jest-dom/vitest'
+import '@testing-library/jest-dom'
 
 import { cleanup } from '@testing-library/react'
 import { afterEach } from 'vitest'
